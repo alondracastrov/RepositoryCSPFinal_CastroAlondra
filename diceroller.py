d595a2ff299fdd9d0a113fa298bfaec9025e5ceb
alondracastrov <105502082+alondracastrov@users.noreply.github.com>
2022-05-25 10:48:02 -0500

    Delete diceroller.py

--- diceroller.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def rolldice(min,max):
-    print("Currently rolling your dice.....")
-    number = random.randint(min,max)
-    print(f"The number that you got is : {number}")
-    
-rolldice(1,6)
-    
